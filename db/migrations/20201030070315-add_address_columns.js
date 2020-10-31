20259a75300fcd4725c840560b46cfa3ceddab68
Alejandro Larumbe <larumbemar@gmail.com>
2020-10-31 14:26:05 +1300

    nuke

--- db/migrations/20201030070315-add_address_columns.js
+++ db/migrations/20201030070315-add_address_columns.js
@@ -2,22 +2,22 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return Promise.all([
-      queryInterface.addColumn('Jam', 'state', {
-        type: Sequelize.STRING(50),
-        allowNull: false,
-      }),
-      queryInterface.addColumn('Jam', 'zipCode', {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      }),
-      queryInterface.addColumn('Jam', 'country', {
-        type: Sequelize.STRING(50),
-        allowNull: false,
-      }),
-      queryInterface.removeColumn('Jam', 'address', {
-      }),
-    ])
+    // return Promise.all([
+    //   queryInterface.addColumn('Jam', 'state', {
+    //     type: Sequelize.STRING(50),
+    //     allowNull: false,
+    //   }),
+    //   queryInterface.addColumn('Jam', 'zipCode', {
+    //     type: Sequelize.INTEGER,
+    //     allowNull: false,
+    //   }),
+    //   queryInterface.addColumn('Jam', 'country', {
+    //     type: Sequelize.STRING(50),
+    //     allowNull: false,
+    //   }),
+    //   queryInterface.removeColumn('Jam', 'address', {
+    //   }),
+    // ])
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -27,14 +27,14 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return Promise.all([
-      queryInterface.removeColumn('Jam', 'country', { id: Sequelize.INTEGER }),
-      queryInterface.addColumn('Jam', 'address', {
-        type: Sequelize.STRING(100),
-        allowNull: false,
-      }),
+    // return Promise.all([
+    //   queryInterface.removeColumn('Jam', 'country', { id: Sequelize.INTEGER }),
+    //   queryInterface.addColumn('Jam', 'address', {
+    //     type: Sequelize.STRING(100),
+    //     allowNull: false,
+    //   }),
 
-    ])
+    // ])
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
